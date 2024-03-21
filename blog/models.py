00b7635488092a04d939cff3149758ec33bd81eb
Satyam Sinha <satyamkumar.sinha@itcinfotech.com>
2024-03-21 12:53:04 +0000

    Finish generic and reverse generic relationships

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericRelation
 # Create your models here.
+
 
 class Tag(models.Model):
   value = models.TextField(max_length=100)
@@ -8,6 +12,21 @@ class Tag(models.Model):
   def __str__(self):
     return self.value
 
+class Comment(models.Model):
+  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  content = models.TextField()
+  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey("content_type", "object_id")
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
+  """
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  ERRORS:
+  blog.Comment.author: (fields.E304) Reverse accessor for 'blog.Comment.author' clashes with reverse accessor for 'blog.Comment.creator'.
+        HINT: Add or change a related_name argument to the definition for 'blog.Comment.author' or 'blog.Comment.creator'.
+  """
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
@@ -19,6 +38,9 @@ class Post(models.Model):
   summary = models.TextField(max_length=500)
   content = models.TextField()
   tags = models.ManyToManyField(Tag, related_name="posts")
+  comments = GenericRelation(Comment)
 
   def __str__(self):
     return self.title
+
+
